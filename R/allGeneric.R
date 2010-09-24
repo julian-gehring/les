753d673524baf2766a6ed6dc6086e49c050d10f9
j.gehring <j.gehring@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-09-24 08:59:17 +0000

    standardize comment headers
    convert all to 70# and standard formatting of functions
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/les@49590 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/allGeneric.R
+++ R/allGeneric.R
@@ -1,13 +1,19 @@
-##################################################
-## generics for class 'Les'
-##################################################
+######################################################################
+## generics for les package
+######################################################################
 
+######################################################################
+## generic ci
+######################################################################
 setGeneric("ci",
            function(object, subset, nBoot=100, conf=0.95,
                     nCores=NULL, ...)
            {standardGeneric("ci")}
            )
 
+######################################################################
+## generic estimate
+######################################################################
 setGeneric("estimate",
            function(object, win, weighting=triangWeight,
                     grenander=TRUE, se=FALSE, minProbes=3,
@@ -15,17 +21,28 @@ setGeneric("estimate",
            {standardGeneric("estimate")}
            )
 
+######################################################################
+## generic regions
+######################################################################
 setGeneric("regions",
            function(object, limit=NULL, minLength=2, maxGap=Inf,
                     verbose=FALSE, ...)
            {standardGeneric("regions")}
            )
 
+
+######################################################################
+## generic threshold
+######################################################################
 setGeneric("threshold",
            function(object, grenander=FALSE, verbose=FALSE, ...)
            {standardGeneric("threshold")}
            )
 
+
+######################################################################
+## generic export
+######################################################################
 setGeneric("export",
            function(object, file, format="bed", chr, range,
                     description="Lambda", strand=".", group="les",
@@ -33,6 +50,10 @@ setGeneric("export",
            {standardGeneric("export")}
            )
 
+
+######################################################################
+## generic chi2
+######################################################################
 setGeneric("chi2",
            function(object, winSize, regions, offset, fdr="lfdr",
                     method, scaling=les:::scaleNorm, nCores=NULL,
